8f095bee60d5993a7096828c83b9b30af993d469
kotgirep <64815155+kotgirep@users.noreply.github.com>
2021-05-26 10:11:25 -0700

    Update DetailArticle.java

--- app/src/main/java/com/example/news/DetailArticle.java
+++ app/src/main/java/com/example/news/DetailArticle.java
@@ -1,11 +1,12 @@
 
 
-import android.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 import dmax.dialog.SpotsDialog;
 
